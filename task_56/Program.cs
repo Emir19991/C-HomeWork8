0c743a177f6eea1d070a1011f0ca400106f3c7fd
Emir Akhmadeshin <akhmadeshin@list.ru>
2023-06-07 20:28:22 +0500

    add homeWork 8

---
+++ task_56/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixFormula(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1); 
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j] + ""} "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int Sumlements(int[,] matrix, int i)
+{
+  int sumLine = matrix[i,0];
+  for (int j = 1; j < matrix.GetLength(1); j++)
+  {
+    sumLine += matrix[i,j];
+  }
+  return sumLine;
+}
+
+
+
+
+int MinSumElements(int[,] matrix)
+{
+    int minSumLine = 0;
+    int sum = Sumlements(matrix, 0);
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int temp = Sumlements(matrix, i);
+        if (sum > temp)
+        {
+            sum = temp;
+            minSumLine = i;
+        }
+    }
+    return minSumLine;     
+}
+
+int[,] array2d = CreateMatrixFormula(4, 4, 1, 9);
+PrintMatrix(array2d);
+
+int minSumElements = MinSumElements(array2d);
+Console.WriteLine($"Наименьшая сумма в строке {minSumElements + 1}");   
+
---
+++ task_58/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixFormula(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1); 
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j] + ""} "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secondMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+
+
+
+
+
+Console.WriteLine("Первая матрица");
+int[,] firstMartrix = CreateMatrixFormula(2, 2, 1, 4);
+PrintMatrix(firstMartrix);
+Console.WriteLine("Вторая матрица");
+int[,] secondMartrix = CreateMatrixFormula(2, 2, 1, 4);
+PrintMatrix(secondMartrix);
+int[,] resultMatrix = CreateMatrixFormula(2, 2, 1, 4);
+MultiplyMatrix(firstMartrix, secondMartrix, resultMatrix);
+Console.WriteLine($"Произведение двух матриц равно: ");
+PrintMatrix(resultMatrix);
